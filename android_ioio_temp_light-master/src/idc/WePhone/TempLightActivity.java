ffc92ff5595db6a4113743e51299969e9f4613d2
Ido michael <Michael.ido@gmail.com>
2015-05-06 10:30:52 +0300

    idols house

--- android_ioio_temp_light-master/src/idc/WePhone/TempLightActivity.java
+++ android_ioio_temp_light-master/src/idc/WePhone/TempLightActivity.java
@@ -6,22 +6,37 @@ import ioio.lib.util.AbstractIOIOActivity;
 import net.mitchtech.ioio.templight.R;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.os.Bundle;
 import android.view.Window;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 
 public class TempLightActivity extends AbstractIOIOActivity {
 	
-	private final int PHOTOCELL_PIN = 35;
-	
+	private final int PHOTOCELL_PIN1 = 35;
+	private final int PHOTOCELL_PIN2 = 34;
+	private final int PHOTOCELL_PIN3 = 33;
+	private final int PHOTOCELL_PIN4 = 32;
+
 	TextView mLightTextView;
 	SeekBar mLightSeekBar;
-	float isConnected;
+	float isConnected1;
+	float isConnected2;
+	float isConnected3;
+	float isConnected4;
+
+	int numOfphones = 0;
+    private WifiP2pManager manager;
+    private Channel channel;
+    private BroadcastReceiver receiver = null; // how to use this reciever?
+    private final IntentFilter intentFilter = new IntentFilter();
+
 	
 	
 	@Override
@@ -29,7 +44,14 @@ public class TempLightActivity extends AbstractIOIOActivity {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.main);
-		
+
+		// initialize intent filter and wifimanager + channel.
+		intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+		intentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+		intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+		intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+		manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+		channel = manager.initialize(this, getMainLooper(), null);
 		mLightTextView = (TextView) findViewById(R.id.tvLight);
 		mLightSeekBar = (SeekBar) findViewById(R.id.sbLight);
 
@@ -37,12 +59,18 @@ public class TempLightActivity extends AbstractIOIOActivity {
 	}
 
 	class IOIOThread extends AbstractIOIOActivity.IOIOThread {
-		private AnalogInput mLightInput;
+		private AnalogInput mLightInput1;
+		private AnalogInput mLightInput2;
+		private AnalogInput mLightInput3;
+		private AnalogInput mLightInput4;
 
 		@Override
 		public void setup() throws ConnectionLostException {
 			try {
-				mLightInput = ioio_.openAnalogInput(PHOTOCELL_PIN);
+				mLightInput1 = ioio_.openAnalogInput(PHOTOCELL_PIN1);
+				mLightInput2 = ioio_.openAnalogInput(PHOTOCELL_PIN2);
+				mLightInput3 = ioio_.openAnalogInput(PHOTOCELL_PIN3);
+				mLightInput4 = ioio_.openAnalogInput(PHOTOCELL_PIN4);
 				enableUi(true);
 			} catch (ConnectionLostException e) {
 				enableUi(false);
@@ -54,15 +82,46 @@ public class TempLightActivity extends AbstractIOIOActivity {
 		public void loop() throws ConnectionLostException {
 			try {
 				
-				final float lightReading = mLightInput.read();
-				isConnected = lightReading * 100;
-				setSeekBar((int) (isConnected));
-				setText(Float.toString((isConnected)));
-				if((int)isConnected < 30) {
-					// Send signal.
-				} else {
-					// Cancel signal.
-				}
+				// Discovering light from sensor1 - should duplicate?
+				final float lightReading1 = mLightInput1.read();
+				isConnected1 = lightReading1 * 100;
+				final float lightReading2 = mLightInput2.read();
+				isConnected2 = lightReading2 * 100;
+				final float lightReading3 = mLightInput3.read();
+				isConnected3 = lightReading3 * 100;
+				final float lightReading4 = mLightInput4.read();
+				isConnected4 = lightReading4 * 100;
+				setSeekBar((int) (isConnected1));
+				setText(Float.toString((isConnected1)));
+				
+				// Starting discovery for peers.
+				manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+				    @Override
+				    public void onSuccess() {
+				    	Toast.makeText(WiFiDirectActivity.this, "Discovery Initiated",
+                                Toast.LENGTH_SHORT).show();
+				    	if((int)isConnected1 < 30) {
+				    	numOfphones++;
+				    	}
+				    	if((int)isConnected2 < 30) {
+					    	numOfphones++;
+				    	}
+				    	if((int)isConnected3 < 30) {
+					    	numOfphones++;
+				    	}
+				    	if((int)isConnected4 < 30) {
+					    	numOfphones++;
+				    	}
+				    	manager.notify(); // how to transfer a message?
+				    	
+				    }
+
+				    @Override
+				    public void onFailure(int reasonCode) {
+				    	Toast.makeText(WiFiDirectActivity.this, "Discovery Failed : " + reasonCode,
+                                Toast.LENGTH_SHORT).show();
+				    }
+				});
 				sleep(10);
 				
 			} catch (InterruptedException e) {
